fd8ef740ef7621c69df448d216b6d053bdbb2b4c
Dan Elbert <dan.elbert@gmail.com>
2016-06-06 17:29:20 -0500

    added templates & static files

--- src/web.rs
+++ src/web.rs
@@ -2,8 +2,8 @@ extern crate iron;
 extern crate router;
 extern crate staticfile;
 extern crate mount;
-extern crate urlencoded;
 extern crate handlebars_iron;
+extern crate rustc_serialize;
 
 extern crate getopts;
 extern crate rusqlite;
@@ -15,12 +15,27 @@ use iron::prelude::*;
 use mount::Mount;
 use router::Router;
 use staticfile::Static;
+use handlebars_iron::{Template, HandlebarsEngine, DirectorySource};
 
+use rustc_serialize::json::{self, ToJson, Json};
+
+use std::collections::BTreeMap;
 use std::path::Path;
-
 
 use pibq::sql;
 use pibq::models::{ConnectionStatus, Reading};
+
+struct Bean {
+    str: String
+}
+
+impl ToJson for Bean {
+    fn to_json(&self) -> Json {
+        let mut m: BTreeMap<String, Json> = BTreeMap::new();
+        m.insert("str".to_string(), self.str.to_json());
+        m.to_json()
+    }
+}
 
 fn main() {
     let mut router = Router::new();
@@ -31,9 +46,23 @@ fn main() {
         .mount("/", router)
         .mount("/assets/", Static::new(Path::new("web/assets")));
 
-    Iron::new(mount).http("0.0.0.0:3000").unwrap();
+    let mut template_engine = HandlebarsEngine::new();
+    template_engine.add(Box::new(DirectorySource::new("web/templates/", ".hbs")));
+
+    if let Err(r) = template_engine.reload() {
+        panic!("{}", r);
+    }
+
+    let mut chain = Chain::new(mount);
+    chain.link_after(template_engine);
+
+    Iron::new(chain).http("0.0.0.0:3000").unwrap();
 
     fn handler(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "OK")))
+        let mut resp = Response::new();
+        let data = Bean { str: "hello world".to_string() };
+        resp.set_mut(Template::new("projects", data)).set_mut(status::Ok);
+        Ok(resp)
+        //Ok(Response::with((status::Ok, "OK")))
     }
 }
