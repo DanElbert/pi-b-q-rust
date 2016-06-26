b7c8f11e4edba680afd7bfa7042e6bd6168f36a5
Dan Elbert <dan.elbert@gmail.com>
2016-06-26 18:21:15 -0500

    startup

--- install.sh
+++ install.sh
@@ -3,15 +3,14 @@
 [ "$UID" -ne 0 ] && echo "You should run this script as a root " && exit 1
 [ ! -x dist/web ] || [ ! -x dist/harvester ] && echo "Executables not found in dist/" && exit 1
 
-apt-get update
-apt-get install -y vim bluetooth bluez bluez-tools pi-bluetooth
-
 mkdir -p /opt/pibq/bin
 
 cp -R web migrations /opt/pibq
 cp dist/web dist/harvester /opt/pibq/bin
 
 chown -R pi /opt/pibq
+
+usermod -a -G www-data pi
 
 cp pi_config/bluetooth_rfcomm.service /lib/systemd/system/
 cp pi_config/pibq_web.service /lib/systemd/system/
@@ -22,5 +21,3 @@ cp pi_config/pibq.env /etc/default/pibq
 systemctl enable bluetooth_rfcomm
 systemctl enable pibq_harvester
 systemctl enable pibq_web
-
-systemctl start pibq_web
